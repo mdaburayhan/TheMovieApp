16aea2b1883f9b108cf49ff0b127e3e1b7185dfd
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-24 16:42:50 +0600

    Created Movie Model class using a online tool jsonschema2pojo

--- app/src/main/java/com/arsoft/themovieapp/model/Movie.java
+++ app/src/main/java/com/arsoft/themovieapp/model/Movie.java
@@ -1,4 +1,92 @@
 package com.arsoft.themovieapp.model;
 
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+// We generated this Movie Model class using a online tool called https://www.jsonschema2pojo.org/
 public class Movie {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("video")
+    @Expose
+    private Boolean video;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+
 }
