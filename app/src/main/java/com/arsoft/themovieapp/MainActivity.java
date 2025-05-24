1fa6f8d5e60ebfdc21f25b79e5c221d658cdc1d6
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-24 19:42:38 +0600

    attach recyclerView,adapter, viewmodel in MainActivity.java and set Internet permission on AndroidManifest.xml and this is the final commit of this project

--- app/src/main/java/com/arsoft/themovieapp/MainActivity.java
+++ app/src/main/java/com/arsoft/themovieapp/MainActivity.java
@@ -7,13 +7,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.arsoft.themovieapp.databinding.ActivityMainBinding;
+import com.arsoft.themovieapp.model.Movie;
+import com.arsoft.themovieapp.view.MovieAdapter;
+import com.arsoft.themovieapp.viewmodel.MainActivityViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Movie> movies;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+
+        getPopularMovies();
+
+        swipeRefreshLayout = binding.swipeLayout;
+        swipeRefreshLayout.setColorSchemeColors(R.color.black);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getPopularMovies();
+            }
+        });
+
+
+
 
     }
+
+    private void getPopularMovies() {
+        viewModel.getAllMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> moviesFromLiveData) {
+                movies = (ArrayList<Movie>) moviesFromLiveData;
+                displayMoviesInRecyclerView();
+            }
+        });
+    }
+
+    private void displayMoviesInRecyclerView() {
+        recyclerView = binding.recyclerView;
+
+        movieAdapter = new MovieAdapter(this, movies);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        // Notify an adapter associated with a RecyclerView
+        // that the underlying dataset has changed
+        movieAdapter.notifyDataSetChanged();
+
+
+    }
+
+
 }
