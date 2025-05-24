f7ead1c386ef33c6b44bdf5b589c56bf63471b78
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-24 12:38:32 +0600

    We have added the Retrofit Serialization and deserialization

--- app/src/main/java/com/arsoft/themovieapp/model/Result.java
+++ app/src/main/java/com/arsoft/themovieapp/model/Result.java
@@ -1,11 +1,31 @@
 package com.arsoft.themovieapp.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class Result {
+
+
+
+    // @SerializedName: Maps the "page" field from the JSON response to the Java variable "pageNumber"
+    // If the JSON key and the Java variable name are the same, using @SerializedName is optional.
+    // @Expose: Indicates that this field should be exposed for JSON serialization and deserialization by Gson
+    @SerializedName("page")
+    @Expose
     private Integer page;
+
+    @SerializedName("total_pages")
+    @Expose
     private Integer totalPages;
+
+    @SerializedName("total_results")
+    @Expose
     private Integer totalResult;
+
+    @SerializedName("results")
+    @Expose
     private List<Movie> result = null;
 
 
